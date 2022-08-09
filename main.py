af48b25b26945b2f1cbfdeafad4f6dd9530da91b
Andrew Huang <ktpss96034@gmail.com>
2022-08-09 14:18:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -163,6 +163,5 @@ def main():
 def unauth():
     return render_template('unauth.html')
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))
-    app.run('0.0.0.0', port=port, debug=True)
+port = int(os.environ.get("PORT", 8080))
+app.run('0.0.0.0', port=port, debug=True)
