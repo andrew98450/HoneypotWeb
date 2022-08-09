bae3d18f6b9cfc44008d8b49ac14835569fdbf79
Andrew Huang <ktpss96034@gmail.com>
2022-08-09 13:50:59 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -163,4 +163,5 @@ def main():
 def unauth():
     return render_template('unauth.html')
 
-app.run('0.0.0.0', port=int(os.environ.get('PORT', 8080)), debug=True)
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=int(os.environ.get('PORT', 8080)), debug=True)
